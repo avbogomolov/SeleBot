631da99aab2e5885772a885bd7b0524712f12896
Andrey Bogomolov <a.bogomolov@icloud.com>
2024-03-28 15:52:16 +0300

    add print current data and code execution time

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from selenium import webdriver
 import time
+import datetime
 import random
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 
+
+# Start of countdown
+start_time = time.time()
 
 
 def random_delay():
-    return int(random.randint(1,30))
+    return int(random.randint(1, 30))
+
+
 def open_site_firefox(site):
     browser = webdriver.Firefox()
     browser.get(site)
@@ -15,15 +20,14 @@ def open_site_firefox(site):
     print(f'I found this on {site} the site in the Firefox browser')
     time.sleep(random_delay())
     browser.find_element(By.ID, "menu-item-565").click()
-    print(f'2 random time is')
     time.sleep(random_delay())
     browser.quit()
+
 
 def open_site_chrome(site):
     browser = webdriver.Chrome()
     browser.get(site)
     browser.find_element(By.ID, "colophon").click()
-    print('i founded it')
     time.sleep(random_delay())
     browser.find_element(By.ID, "menu-item-565").click()
     time.sleep(random_delay())
@@ -31,9 +35,19 @@ def open_site_chrome(site):
 
 
 open_site_firefox('https://ironchampion.ru')
-
 open_site_chrome('https://ironchampion.ru')
 
 
+current_data = datetime.datetime.now()
+
+
+# End of countdown
+end_time = time.time()
+
+
+# Calculating execution time
+execution_time = round(end_time - start_time, 2)
+
+print(f"code execution time: {execution_time} second and current data is: {current_data}")
 
 
